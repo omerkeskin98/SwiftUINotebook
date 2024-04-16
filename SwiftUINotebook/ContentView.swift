c8383e258ceef4ca3b6fc9c4115462b1cc7313ec
Omer Faruk Keskin <keskinomerf@gmail.com>
2024-04-16 08:12:46 +0300

    Initial Commit

---
+++ SwiftUINotebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUINotebook
+//
+//  Created by Omer Keskin on 16.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUINotebook/SwiftUINotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINotebookApp.swift
+//  SwiftUINotebook
+//
+//  Created by Omer Keskin on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
